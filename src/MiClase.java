bcd053008b9525b544fd7c7c7f280ed2d3982d3b
fabricio18 <ivethsabillon11@gmail.com>
2014-06-16 12:04:57 -0600

    Update MiClase.java

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,20 +1,23 @@
 
 public class MiClase
-{// Ejercicios realizado en grupo con iveth.
+{// Ejercicios realizado en grupo con iveth sabillon
 	//devuelve la suma de a y b
 	static int sumarNumeros(int a, int b)
+	// con esta operacion podemos hacer el calculo de la suma de dos variables.
 	{
 		return a+b;
 	}
 	
 	//devuelve la resta de a y b
 	static int restarNumeros(int a, int b)
+	// con esta operacion podemos hacer el calculo de la resta de dos variables.
 	{
 		return a-b;
 	}
 	
 	//devuelve la multiplicación de a y b
 	static int multiplicarNumeros(int a, int b)
+	// con esta operacion podemos hacer el calculo de la multiplicacion de dos variables.
 	{
 		return a*b;
 	}
@@ -22,6 +25,8 @@ public class MiClase
 	//devuelve true si num es par, de lo contrario, devuelve false
 	
 	static boolean esPar(int num){
+		//con esta operacion podemos realizar el calculo de la variable y verificar si el numero es par o divisible de lo contrario el resultado
+		//va ser falso.
 	if(num%2==0)
         return true;
      else
@@ -29,6 +34,7 @@ public class MiClase
 }
 	
 	//devuelve el numero mayor entre a, b o c
+	//con esta operacion simplemente podemos ver el numero mayor entre los tres numeros que podamos introducir.
 	static int getMayor(int a, int b, int c)
 	{
 		if(a > b)
@@ -43,6 +49,7 @@ public class MiClase
 	}
 	
 	//devuelve num elevado a la potencia
+	//con esta operacion se declara una variable y se da como resultado  el operador logico.
 	static int potencia(int num, int potencia)
 	{
 		int resultado = (int) Math.pow(num, potencia);
@@ -50,6 +57,7 @@ public class MiClase
 	}
 	
 	//devuelve el numero mayor del arreglo
+	//es la calculo de la suma de variables del arreglo.
 	static int sumarArreglo(int arreglo[])
 	{
 		int sumarArreglo=0; 
@@ -61,6 +69,7 @@ public class MiClase
 	}
 	
 	//devuelve el numero mayor de un arreglo
+	// se declaran variable para obtener el numero mayor.
 	static int getMayorArreglo(int arreglo[])
 	{
 		int imayor =0;
@@ -73,6 +82,7 @@ public class MiClase
 	
 	
 	//devuelve el numero menor de un arreglo
+	//se declaran variable para obtener el numero menor.
 	static int getMenorArreglo(int arreglo[])
 	{
 	int imenor =0;
@@ -83,6 +93,7 @@ public class MiClase
 	}
 	
 	//devuelve true si encuentra cadena dentro arreglo
+	//ccn esto se obtiene una variable que si con tiene texto nos va a dar un resultado verdadero de lo contrario nos va dar falso.
 	static boolean existe(String arreglo[],String cadena)
 	{
 		for(int x =0; x<arreglo.length; x++)
