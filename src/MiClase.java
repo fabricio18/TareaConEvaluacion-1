6484f269f0b190088a0eff76227e4357539b6f1b
Alumno <alumno@localhost.localdomain>
2014-06-16 11:43:58 -0600

    modificaciones

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,6 +1,6 @@
 
 public class MiClase
-{
+{// Ejercicios realizado en grupo con iveth.
 	//devuelve la suma de a y b
 	static int sumarNumeros(int a, int b)
 	{
@@ -20,51 +20,78 @@ public class MiClase
 	}
 	
 	//devuelve true si num es par, de lo contrario, devuelve false
-	static boolean esPar(int num)
-	{
-	if num=(esPar);
-	return true;
-	else;
-	return false; 
-		
-	}
+	
+	static boolean esPar(int num){
+	if(num%2==0)
+        return true;
+     else
+         return false;
+}
 	
 	//devuelve el numero mayor entre a, b o c
 	static int getMayor(int a, int b, int c)
 	{
-		return -1;
+		if(a > b)
+	           if(a>c)
+	              return a;
+	           else
+	              return c;
+	        else if(b>c)
+	              return b;
+	                else
+	              return c;
 	}
 	
 	//devuelve num elevado a la potencia
 	static int potencia(int num, int potencia)
 	{
-		return-1;
+		int resultado = (int) Math.pow(num, potencia);
+		return resultado;
 	}
 	
 	//devuelve el numero mayor del arreglo
 	static int sumarArreglo(int arreglo[])
 	{
-		return -1;
+		int sumarArreglo=0; 
+		for (int i=0; i< arreglo.length; i++) 
+		sumarArreglo+=arreglo[i]; 
+		
+	
+		return sumarArreglo;
 	}
 	
 	//devuelve el numero mayor de un arreglo
 	static int getMayorArreglo(int arreglo[])
 	{
-		return -1;
-	}
+		int imayor =0;
+		for(int i=1;i<arreglo.length;i++)
+			if(arreglo[i]>arreglo[imayor])
+				imayor = i;
+		return arreglo[imayor];
+
+}
+	
 	
 	//devuelve el numero menor de un arreglo
 	static int getMenorArreglo(int arreglo[])
 	{
-		return -1;
+	int imenor =0;
+	for(int i=1;i<arreglo.length;i++)
+		if(arreglo[i]<arreglo[imenor])
+			imenor = i;
+	return arreglo[imenor];
 	}
 	
 	//devuelve true si encuentra cadena dentro arreglo
 	static boolean existe(String arreglo[],String cadena)
 	{
+		for(int x =0; x<arreglo.length; x++)
+		{
+			if(arreglo[x].equals(cadena))
+				return true;
+		}
 		return false;
 	}
-	
 	public static void main(String args[])
 	{
 		System.out.print("hola");
